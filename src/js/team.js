346e14a392698a3f07eca608c7d33be38d22021c
Natalia <nata@host>
2021-06-14 12:34:24 +0500

    класс Team готов

---
+++ src/js/team.js
@@ -0,0 +1,27 @@
+class Team {
+    constructor() {
+        this.member = new Set();
+    }
+
+    add(character) {
+
+        if (!this.member.has(character)) {
+            this.member.add(character);
+        } else throw new Error('такой персонаж уже существует');
+    }
+
+
+    addAll(...characters) {
+        console.log(characters);
+
+        for (let i = 0; i < characters.length; i++) {
+            this.member.add(characters[i]);
+        };
+
+    }
+
+    toArray() {
+        this.member = [...this.member];
+    }
+
+}
