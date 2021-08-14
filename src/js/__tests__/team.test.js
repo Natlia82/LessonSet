66a1fe85768264be248313f64e2397bda1b76f05
Natalia <nata@host>
2021-08-14 10:24:00 +0500

    lint

--- src/js/__tests__/team.test.js
+++ src/js/__tests__/team.test.js
@@ -1,28 +1,28 @@
 import Team from '../team';
 
 test('one character', () => {
-    const rezult = new Team();
-    rezult.add('one');
-    rezult.toArray();
-    expect(rezult).toEqual({ "member": ["one"] });
+  const rezult = new Team();
+  rezult.add('one');
+  rezult.toArray();
+  expect(rezult).toEqual({ member: ['one'] });
 });
 
 test('many character', () => {
-    const rezult = new Team();
-    rezult.addAll('one', 'two', 'three');
-    rezult.toArray();
-    expect(rezult).toEqual({ "member": ["one", "two", "three"] });
+  const rezult = new Team();
+  rezult.addAll('one', 'two', 'three');
+  rezult.toArray();
+  expect(rezult).toEqual({ member: ['one', 'two', 'three'] });
 });
 
 test('replay character', () => {
-    const rezult = new Team();
-    rezult.addAll('one', 'two', 'one', 'three');
-    rezult.toArray();
-    expect(rezult).toEqual({ "member": ["one", "two", "three"] });
+  const rezult = new Team();
+  rezult.addAll('one', 'two', 'one', 'three');
+  rezult.toArray();
+  expect(rezult).toEqual({ member: ['one', 'two', 'three'] });
 });
 
 test('Boom!', () => {
-    const rezult = new Team();
-    rezult.add('one');
-    expect(() => { rezult.add('one'); }).toThrow('такой персонаж уже существует');
+  const rezult = new Team();
+  rezult.add('one');
+  expect(() => { rezult.add('one'); }).toThrow('такой персонаж уже существует');
 });
--- src/js/team.js
+++ src/js/team.js
@@ -1,27 +1,24 @@
 export default class Team {
-    constructor() {
-        this.member = new Set();
+  constructor() {
+    this.member = new Set();
+  }
+
+  add(character) {
+    if (!this.member.has(character)) {
+      this.member.add(character);
+    } else throw new Error('такой персонаж уже существует');
+  }
+
+
+  addAll(...characters) {
+    // console.log(characters);
+
+    for (let i = 0; i < characters.length; i += 1) {
+      this.member.add(characters[i]);
     }
+  }
 
-    add(character) {
-
-        if (!this.member.has(character)) {
-            this.member.add(character);
-        } else throw new Error('такой персонаж уже существует');
-    }
-
-
-    addAll(...characters) {
-        console.log(characters);
-
-        for (let i = 0; i < characters.length; i++) {
-            this.member.add(characters[i]);
-        };
-
-    }
-
-    toArray() {
-        this.member = [...this.member];
-    }
-
+  toArray() {
+    this.member = [...this.member];
+  }
 }
